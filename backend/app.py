5ad0df665c53779e6c9f669adc6dc6a8b525fd37
lucasgagne <lucasgagne@berkeley.edu>
2026-04-07 13:14:46 -0700

    changed the CORS connections

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={
     r"/*": {
-        "origins": ["https://full-stack-dummy.vercel.app"]
+        "origins": ["https://fullstack-dataworld.vercel.app"]
     }
 })
 @app.route('/')
